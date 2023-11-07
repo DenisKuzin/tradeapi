444d27db13c0f3d4294d33d4a7f40a8db50e3f14
Denis Kuzin <digiholiq@gmail.com>
2023-11-07 22:09:19 +0300

    prettyfy

--- gen.sh
+++ gen.sh
@@ -1,15 +1,73 @@
 #!/bin/bash
 set -x
 export PATH=$PATH:$(go env GOPATH)/bin
-protoc --go_out=/tradeapi --go_opt=paths=source_relative --go_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi --go-grpc_out=/tradeapi --go-grpc_opt=paths=source_relative --go-grpc_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi proto/tradeapi/v1/common.proto
-protoc --go_out=/tradeapi --go_opt=paths=source_relative --go_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi --go_opt=Mproto/tradeapi/v1/candles.proto=github.com/DenisKuzin/tradeapi --go-grpc_out=/tradeapi --go-grpc_opt=paths=source_relative --go-grpc_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi --go-grpc_opt=Mproto/tradeapi/v1/candles.proto=github.com/DenisKuzin/tradeapi proto/tradeapi/v1/candles.proto
-protoc --go_out=/tradeapi --go_opt=paths=source_relative --go_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi --go_opt=Mproto/tradeapi/v1/portfolios.proto=github.com/DenisKuzin/tradeapi --go_opt=Mproto/tradeapi/v1/orders.proto=github.com/DenisKuzin/tradeapi --go_opt=Mproto/tradeapi/v1/events.proto=github.com/DenisKuzin/tradeapi --go-grpc_out=/tradeapi --go-grpc_opt=paths=source_relative --go-grpc_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi --go-grpc_opt=Mproto/tradeapi/v1/portfolios.proto=github.com/DenisKuzin/tradeapi --go-grpc_opt=Mproto/tradeapi/v1/orders.proto=github.com/DenisKuzin/tradeapi --go-grpc_opt=Mproto/tradeapi/v1/events.proto=github.com/DenisKuzin/tradeapi proto/tradeapi/v1/events.proto
-protoc --go_out=/tradeapi --go_opt=paths=source_relative --go_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi --go_opt=Mproto/tradeapi/v1/orders.proto=github.com/DenisKuzin/tradeapi --go-grpc_out=/tradeapi --go-grpc_opt=paths=source_relative --go-grpc_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi --go-grpc_opt=Mproto/tradeapi/v1/orders.proto=github.com/DenisKuzin/tradeapi proto/tradeapi/v1/orders.proto
-protoc --go_out=/tradeapi --go_opt=paths=source_relative --go_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi --go_opt=Mproto/tradeapi/v1/portfolios.proto=github.com/DenisKuzin/tradeapi --go-grpc_out=/tradeapi --go-grpc_opt=paths=source_relative --go-grpc_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi --go-grpc_opt=Mproto/tradeapi/v1/portfolios.proto=github.com/DenisKuzin/tradeapi proto/tradeapi/v1/portfolios.proto
-protoc --go_out=/tradeapi --go_opt=paths=source_relative --go_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi --go_opt=Mproto/tradeapi/v1/security.proto=github.com/DenisKuzin/tradeapi --go-grpc_out=/tradeapi --go-grpc_opt=paths=source_relative --go-grpc_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi --go-grpc_opt=Mproto/tradeapi/v1/security.proto=github.com/DenisKuzin/tradeapi proto/tradeapi/v1/security.proto
-nprotoc --go_out=/tradeapi --go_opt=paths=source_relative --go_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi --go_opt=Mproto/tradeapi/v1/stops.proto=github.com/DenisKuzin/tradeapi --go-grpc_out=/tradeapi --go-grpc_opt=paths=source_relative --go-grpc_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi --go-grpc_opt=Mproto/tradeapi/v1/stops.proto=github.com/DenisKuzin/tradeapi proto/tradeapi/v1/stops.proto
+protoc --go_out=/tradeapi --go_opt=paths=source_relative \
+       --go_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi \
+       --go-grpc_out=/tradeapi --go-grpc_opt=paths=source_relative \
+       --go-grpc_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi \
+       proto/tradeapi/v1/common.proto
 
-protoc --go_out=/tradeapi --go_opt=paths=source_relative --go_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi --go_opt=Mproto/tradeapi/v1/candles.proto=github.com/DenisKuzin/tradeapi --go_opt=Mgrpc/tradeapi/v1/candles.proto=github.com/DenisKuzin/tradeapi --go-grpc_out=/tradeapi --go-grpc_opt=paths=source_relative --go-grpc_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi --go-grpc_opt=Mgrpc/tradeapi/v1/candles.proto=github.com/DenisKuzin/tradeapi --go-grpc_opt=Mproto/tradeapi/v1/candles.proto=github.com/DenisKuzin/tradeapi grpc/tradeapi/v1/candles.proto
+protoc --go_out=/tradeapi --go_opt=paths=source_relative \
+       --go_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi \
+       --go_opt=Mproto/tradeapi/v1/candles.proto=github.com/DenisKuzin/tradeapi \
+       --go-grpc_out=/tradeapi --go-grpc_opt=paths=source_relative \
+       --go-grpc_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi \
+       --go-grpc_opt=Mproto/tradeapi/v1/candles.proto=github.com/DenisKuzin/tradeapi \
+       proto/tradeapi/v1/candles.proto
+
+protoc --go_out=/tradeapi --go_opt=paths=source_relative \
+       --go_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi \
+       --go_opt=Mproto/tradeapi/v1/portfolios.proto=github.com/DenisKuzin/tradeapi \
+       --go_opt=Mproto/tradeapi/v1/orders.proto=github.com/DenisKuzin/tradeapi \
+       --go_opt=Mproto/tradeapi/v1/events.proto=github.com/DenisKuzin/tradeapi \
+       --go-grpc_out=/tradeapi --go-grpc_opt=paths=source_relative \
+       --go-grpc_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi \
+       --go-grpc_opt=Mproto/tradeapi/v1/portfolios.proto=github.com/DenisKuzin/tradeapi \
+       --go-grpc_opt=Mproto/tradeapi/v1/orders.proto=github.com/DenisKuzin/tradeapi \
+       --go-grpc_opt=Mproto/tradeapi/v1/events.proto=github.com/DenisKuzin/tradeapi \
+       proto/tradeapi/v1/events.proto
+
+protoc --go_out=/tradeapi --go_opt=paths=source_relative \
+       --go_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi \
+       --go_opt=Mproto/tradeapi/v1/orders.proto=github.com/DenisKuzin/tradeapi \
+       --go-grpc_out=/tradeapi --go-grpc_opt=paths=source_relative \
+       --go-grpc_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi \
+       --go-grpc_opt=Mproto/tradeapi/v1/orders.proto=github.com/DenisKuzin/tradeapi \
+       proto/tradeapi/v1/orders.proto
+
+protoc --go_out=/tradeapi --go_opt=paths=source_relative \
+       --go_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi \
+       --go_opt=Mproto/tradeapi/v1/portfolios.proto=github.com/DenisKuzin/tradeapi \
+       --go-grpc_out=/tradeapi --go-grpc_opt=paths=source_relative \
+       --go-grpc_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi \
+       --go-grpc_opt=Mproto/tradeapi/v1/portfolios.proto=github.com/DenisKuzin/tradeapi \
+       proto/tradeapi/v1/portfolios.proto
+
+protoc --go_out=/tradeapi --go_opt=paths=source_relative \
+       --go_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi \
+       --go_opt=Mproto/tradeapi/v1/security.proto=github.com/DenisKuzin/tradeapi \
+       --go-grpc_out=/tradeapi --go-grpc_opt=paths=source_relative \
+       --go-grpc_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi \
+       --go-grpc_opt=Mproto/tradeapi/v1/security.proto=github.com/DenisKuzin/tradeapi \
+       proto/tradeapi/v1/security.proto
+
+nprotoc --go_out=/tradeapi --go_opt=paths=source_relative \
+	--go_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi \
+	--go_opt=Mproto/tradeapi/v1/stops.proto=github.com/DenisKuzin/tradeapi \
+	--go-grpc_out=/tradeapi --go-grpc_opt=paths=source_relative \
+	--go-grpc_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi \
+	--go-grpc_opt=Mproto/tradeapi/v1/stops.proto=github.com/DenisKuzin/tradeapi \
+	proto/tradeapi/v1/stops.proto
+
+protoc --go_out=/tradeapi --go_opt=paths=source_relative \
+       --go_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi \
+       --go_opt=Mproto/tradeapi/v1/candles.proto=github.com/DenisKuzin/tradeapi \
+       --go_opt=Mgrpc/tradeapi/v1/candles.proto=github.com/DenisKuzin/tradeapi \
+       --go-grpc_out=/tradeapi --go-grpc_opt=paths=source_relative \
+       --go-grpc_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi \
+       --go-grpc_opt=Mgrpc/tradeapi/v1/candles.proto=github.com/DenisKuzin/tradeapi \
+       --go-grpc_opt=Mproto/tradeapi/v1/candles.proto=github.com/DenisKuzin/tradeapi \
+       grpc/tradeapi/v1/candles.proto
 
 protoc --go_out=/tradeapi --go_opt=paths=source_relative \
        --go_opt=Mproto/tradeapi/v1/common.proto=github.com/DenisKuzin/tradeapi \
